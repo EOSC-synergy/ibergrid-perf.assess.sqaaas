334b88c93fd982d2b6f3f91c24fe501e7176f541
orviz <orviz@ifca.unican.es>
2024-02-16 10:57:33 +0100

    Add JePL file structure

--- .sqa/script.whiny-chocolate-anteater.sh
+++ .sqa/script.whiny-chocolate-anteater.sh
@@ -1,8 +1,9 @@
 # SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+# SPDX-FileContributor: Pablo Orviz <orviz@ifca.unican.es>
 #
 # SPDX-License-Identifier: GPL-3.0-only
 
 (
 cd github.com/EOSC-synergy/ibergrid-perf &&
-    hadolint service_backup/Dockerfile service_certificate/Dockerfile service_postfix/Dockerfile service_rproxy/Dockerfile --failure-threshold error
+    git rev-parse --is-inside-work-tree
 )
